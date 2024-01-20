dacb8c91ad5514d3f229effcf696d6a50da721f4
deepsikhabehera <deepsikhabehera@kgpian.iitkgp.ac.in>
2024-01-20 12:12:26 +0530

    Concurrent server

--- server.py
+++ server.py
@@ -1,38 +1,35 @@
 import socket
 import os
 
-#fetch server_id from environment variable
+# Fetch server_id from environment variable
 server_id = os.environ.get('SERVER_ID', 'Unknown')
 
-from _thread import *
-import threading
- 
-print_lock = threading.Lock()
- 
-# thread function
-def threaded(c):
+# Thread function
+def handle_client(c):
     while True:
         message = c.recv(1024).decode()
-        print('Received message: ', message)
-        if(message == 'EXIT'):
+        print(f"Received message: {message}")
+        
+        if message == 'EXIT':
             print("Closing connection!")
-            print_lock.release()
             break
 
-        # parse message: say request_type and request_method
+        # Parse message: say request_type and request_method
         request_type, request_method = message.split(' ')
-        if(request_type == 'GET'):
-            if(request_method == '/home'):
+        
+        if request_type == 'GET':
+            if request_method == '/home':
                 response = home()
-            elif(request_method == '/heartbeat'):
+            elif request_method == '/heartbeat':
                 response = heartbeat()
             else:
                 response = not_found()
         else:
             response = not_found()
-        
-        print('Sending response: ', response)
-        c.send(response.encode())   
+
+        print('Sending response:', response)
+        c.send(response.encode())
+
     c.close()
 
 def home():
@@ -48,26 +45,44 @@ def not_found():
     return response
 
 def main():
+    host = '127.0.0.1'
     port = 5002
+
     # Create a socket object
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("Socket successfully created")   
+    print("Socket successfully created")
 
-    s.bind(('', port))         
-    print ("socket binded to %s" %(port)) 
-    
-    # put the socket into listening mode 
-    s.listen(5)     
-    print ("socket is listening")        
+    # Bind the socket to a specific address and port
+    s.bind((host, port))
+    print("Socket binded to %s:%s" % (host, port))
 
-    while True: 
-        c, addr = s.accept()   
+    # Put the socket into listening mode
+    s.listen(5)
+    print("Socket is listening")
 
-        print_lock.acquire()
-        print('Connected to :', addr[0], ':', addr[1])
+    try:
+        while True:
+            # Wait for a connection
+            c, addr = s.accept()
+            print('Connected to:', addr[0], ':', addr[1])
 
-        start_new_thread(threaded, (c,))
-    # s.close()
+            # Fork a new process to handle the client
+            child_pid = os.fork()
+
+            if child_pid == 0:
+                # This is the child process
+                s.close()  # Close the socket in the child process
+                handle_client(c)
+                os._exit(0)  # Exit the child process
+
+            # This is the parent process
+            c.close()  # Close the client socket in the parent process
+
+    except KeyboardInterrupt:
+        print("Server shutting down...")
+
+    finally:
+        s.close()
 
 if __name__ == '__main__':
     main()
