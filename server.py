f505ffadfab813f6e585fe48c409ef9f9fe16a5d
deepsikhabehera <deepsikhabehera@kgpian.iitkgp.ac.in>
2024-01-20 12:00:58 +0530

    Multi-threaded server

--- server.py
+++ server.py
@@ -4,6 +4,37 @@ import os
 #fetch server_id from environment variable
 server_id = os.environ.get('SERVER_ID', 'Unknown')
 
+from _thread import *
+import threading
+ 
+print_lock = threading.Lock()
+ 
+# thread function
+def threaded(c):
+    while True:
+        message = c.recv(1024).decode()
+        print('Received message: ', message)
+        if(message == 'EXIT'):
+            print("Closing connection!")
+            print_lock.release()
+            break
+
+        # parse message: say request_type and request_method
+        request_type, request_method = message.split(' ')
+        if(request_type == 'GET'):
+            if(request_method == '/home'):
+                response = home()
+            elif(request_method == '/heartbeat'):
+                response = heartbeat()
+            else:
+                response = not_found()
+        else:
+            response = not_found()
+        
+        print('Sending response: ', response)
+        c.send(response.encode())   
+    c.close()
+
 def home():
     response = f"message: Hello from Server: {server_id}, status: successful, code: 200"
     return response
@@ -28,33 +59,15 @@ def main():
     # put the socket into listening mode 
     s.listen(5)     
     print ("socket is listening")        
-    """
-    TODO: convert iterative server to concurrent server
-    """
+
     while True: 
-        # Establish connection with client. 
-        c, addr = s.accept()     
-        print ('Received connection request from: ', addr )
-        # c.send('/Connected!'.encode()) 
-        message = c.recv(1024).decode()
-        print('Received message: ', message)
-        # parse message: say request_type and request_method
-        request_type, request_method = message.split(' ')
-        # TODO: handle EXIT message to close socket in concurrent server
-        if(request_type == 'GET'):
-            if(request_method == '/home'):
-                response = home()
-            elif(request_method == '/heartbeat'):
-                response = heartbeat()
-            else:
-                response = not_found()
-        else:
-            response = not_found()
-        
-        print('Sending response: ', response)
-        c.send(response.encode())       
-        c.close()
-        break
+        c, addr = s.accept()   
+
+        print_lock.acquire()
+        print('Connected to :', addr[0], ':', addr[1])
+
+        start_new_thread(threaded, (c,))
+    # s.close()
 
 if __name__ == '__main__':
     main()
