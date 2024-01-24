d030b3f73a00dde09721cb2190d25c8b52ff8374
Vibhu <vibhuyadav41002@gmail.com>
2024-01-24 20:58:45 +0530

    debugging, docker compose-ing

--- asgn1/server/server.py
+++ asgn1/server/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, json
-import sys
+import sys, os
 
-serverID = None
+serverID = os.environ['serverID']
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -23,14 +23,27 @@ def heartbeat():
         response = None,
         status = 200
     )
+    
+@app.route("/<path>", methods = ["GET"])
+def other(path):
+    print(path)
+    
+    return app.response_class(
+        response = json.dumps({
+            "message" : f"<Error> '/{path}' endpoint does not exist in server replicas",
+            "status" : "failure"
+            }),
+        status = 400
+    )
+
 
 if __name__ == "__main__":
     
-    if(len(sys.argv) != 2):
-        print("Error! Usage : python3 server.py <ServerID>")
-        exit(0)
+    # if(len(sys.argv) != 2):
+    #     print("Error! Usage : python3 server.py <ServerID>")
+    #     exit(0)
     
-    serverID = sys.argv[1]
+    # serverID = sys.argv[1]
         
     app.run( host = "0.0.0.0", port = 5000, debug = True)
     
